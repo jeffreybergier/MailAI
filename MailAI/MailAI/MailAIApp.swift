dcd4bd7922ce3084ad4930f95539683800276ddc
Jeffrey Bergier <jeffburg@jeffburg.com>
2025-08-06 21:52:10 +0900

    [bootstrap] initial commit

---
+++ MailAI/MailAI/MailAIApp.swift
@@ -0,0 +1,44 @@
+//
+// GPLv3 License Notice
+//
+// Copyright (c) 2025 Jeffrey Bergier
+//
+// This file is part of MailAI.
+// MailAI is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+// MailAI is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with MailAI. If not, see <https://www.gnu.org/licenses/>.
+//
+
+import SwiftUI
+
+@main
+struct MailAIApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
